43330b9fc660bb566ffd5be9445a145b3a78f195
Ordin Zabek <lukelove216@gmail.com>
2021-02-25 10:27:31 +0800

    attempting Bootsnap fix for heroku

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-unless Rails.env.production?
-  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-end
+# unless Rails.env.production?
+#   require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+# end
