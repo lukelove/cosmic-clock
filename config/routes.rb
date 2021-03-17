2c75b9a5106806328d38bce12a7acb1198a2983a
Ordin Zabek <lukelove216@gmail.com>
2021-03-17 20:40:40 +0800

    Background Image

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "astro#index"
+  get '/moon', to: "astro#moon"
+  get '/sun', to: "astro#sun"
 
 end
