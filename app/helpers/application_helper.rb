e4dd1d2a09743b908e046398ac89f5d072b30f66
Ordin Zabek <lukelove216@gmail.com>
2021-03-18 13:08:51 +0800

    renaming nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   def app_title
-    return 'Windows In Time' if @app == 'windows'
-    return 'Tattvas' if @app == 'sun'
+    return 'Overlapping Windows In Time' if @app == 'windows'
+    return 'Sun Emanations' if @app == 'sun'
     return 'Planetary Hours' if @app == 'moon'
   end
 
